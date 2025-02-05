29b518aa225d2d95c94eee269e79bab2c4ac445a
root <stevemkrenz@gmail.com>
2025-02-04 21:20:25 -0600

    Some code cleanup

--- main/main.ino
+++ main/main.ino
@@ -11,33 +11,6 @@ Adafruit_SSD1306 display;
 ThreeBody threeBody;
 Timer timer;
 
-
-//outputs
-Output* outputJack0 = new OutputJackMCP4728(MCP4728_CHANNEL_A);
-Output* outputJack1 = new OutputJackMCP4728(MCP4728_CHANNEL_B);
-Output* outputJack2 = new OutputJackMCP4728(MCP4728_CHANNEL_C);
-Output* outputJack3 = new OutputJackMCP4728(MCP4728_CHANNEL_D);
-
-//IOmaps
-//IOMap iom;
-
-int count;
-bool blinkOn;
-
-bool rte0IsPressed;
-bool rte0CWTurn;
-bool rte0CCWTurn;
-bool prevRte0CWTurn;
-int rte0Value = 0;
-
-
-double redrawTime = .05;
-double redrawTimer = 0;
-
-double testOutput = 0;
-
-RotaryEncoder* r;
-
 void setup() {
     //definitely need this shit. Pass the display to the 
     //other classes and stuff so they're all referencing
@@ -59,8 +32,11 @@ void setup() {
     Program::AddSystemInput(new InputJackTeensyAnalog(CCTC("Input Jack 4"),23));
     //Program::AddSystemInput(new RotaryEncoder(CCTC("Rotary Encoder"),9,10,11));
 
-
-    //Program::AddSystemOutput(Output *newOutput)
+    //outputs
+    Program::AddSystemOutput(new OutputJackMCP4728(MCP4728_CHANNEL_A));
+    Program::AddSystemOutput(new OutputJackMCP4728(MCP4728_CHANNEL_B));
+    Program::AddSystemOutput(new OutputJackMCP4728(MCP4728_CHANNEL_C));
+    Program::AddSystemOutput(new OutputJackMCP4728(MCP4728_CHANNEL_D));
 
 
     //setup what the inputs and outputs look like, eventually this shit
@@ -96,37 +72,15 @@ void setup() {
         for(;;); // Don't proceed, loop forever
     }
 
-
-    blinkOn = false;
-
-
     //initialize threeBody and other junk last because it FUCKS UP everything else
     //before it in memory like if there are pointers or anything interesting.
     // put your setup code here, to run once:
     threeBody = ThreeBody::CreateRandom(3,1);
-    //delay(100);
     Timer::Reset();
 }
 
 void loop() {
-
     //three body simulation. It's pretty important, chump:
     threeBody.Run();
-
-    //rte0IsPressed = !digitalRead(10);
-    //rte0CWTurn = !digitalRead(11);
-    //rte0CCWTurn = !digitalRead(12);
-
-    //pint some input for more testing
-    //Serial.println((*inputJack0).GetValue());
-
-    //code to redraw the screen. Pretty fucking janky. This should be 
-    //fixed by someone who knows what they're doing at some point.
-    
-    //write to some random LEDs for testing buttons and shit.
-    //Program::SystemInputs()[2]->SampleValue();
     digitalWriteFast(13,Program::SystemInputs()[2]->Value());
-    //digitalWriteFast(13, forwardButton->GetValue() || backButton->GetValue() || rte0IsPressed ? HIGH : LOW);
-    //digitalWriteFast(7, rte0CCWTurn ? HIGH : LOW);
-    //digitalWriteFast(8, rte0CWTurn ? HIGH : LOW);
 }
